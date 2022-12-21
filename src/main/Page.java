a27221797a686a73efe80be86d3ce18067a298c9
Eugene Hong <awidesky@naver.com>
2022-12-21 23:16:59 +0900

    add Page class for represent a page

---
+++ src/main/Page.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Page {
+
+	public String text;
+	public final long pageNum;
+	
+	public Page(String text, long pageNum) {
+		this.text = text;
+		this.pageNum = pageNum;
+	}
+}
